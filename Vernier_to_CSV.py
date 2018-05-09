b73be9e6e9e3d5d2f04898edaf23d4fe7c529460
Matěj Melichna <melimahu@gmail.com>
2018-05-09 21:37:07 +0200

    Added Vernier_to_CSV.py script for parsing values from vernier generated txt to csv for further manipulation (dbase import etc..)

---
+++ Vernier_to_CSV.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import csv
+
+inPath = str("/home/melimat/Downloads/Trefny.txt")
+outPath = str("/home/melimat/Downloads/myCsv.csv")
+
+
+class csvFile:
+
+    def open(self, path):
+        csvFile = open(path, "w")
+        return(csvFile)
+
+    def writeRow(self, csvFile, rowText=str(), splitter=str()):
+        writer = csv.writer(csvFile)
+        rowArray = rowText.split(splitter)
+        rowToWrite = []
+        for eachElement in rowArray:
+            rowToWrite.append(eachElement)
+        writer.writerow(rowToWrite)
+
+
+class txtFile:
+
+    def open(self, path):
+        txtFile = open(path, "r")
+        return(txtFile)
+
+
+def parse():
+
+    splitter = "\t"
+    unitLine = 6
+    dataStartLine = 8
+
+    inp = txtFile()
+    inputFile = inp.open(inPath)
+    out = csvFile()
+    outputFile = out.open(outPath)
+
+    i = int(0)
+    for eachLine in inputFile:
+        i += 1
+        if (i == unitLine) or (i >= dataStartLine):
+            out.writeRow(outputFile, eachLine, splitter)
+
+    inputFile.close()
+    outputFile.close()
+
+
+parse()
