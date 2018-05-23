85fde7229cac7199cefcac2af544ec68e0aaef7b
Matěj Melichna <2017-d-melichna@KNIHOVNA-2.GARAB>
2018-05-23 14:29:53 +0200

    Started object rework on Multiple-curves.py

--- Obj-Multiple-curves.py
+++ Obj-Multiple-curves.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-global pathsArray
-pathsArray = []
 
 class getInput:
     def readInput(self):
@@ -38,79 +36,92 @@ class getInput:
         return(labelsLine)
 
 class processData:
-    def parseValues(self, path=str(), splitter=str(), dataStart=int()):
-        dataFile = open(path, "r")
-        dataArray = []
-        xArray = []
-        yArray = []
+    def __init__(self, splitter):
+        self.splitter = splitter
+        self.pathsToFiles = []
+
+    def parseData(self, dataStart=int(), labelsRow = int(), path = str()):
+        self.path = path()
+        dataFile = open(self.path, "r")
+        self.pathsToFiles.append(self.path)
+        self.xArray = []
+        self.yArray = []
+        self.labelsArray = []
         lineNumber = int(0)
         for eachLine in dataFile:
             lineNumber += 1
-            lineArray = eachLine.split(splitter)
-            if (lineNumber >= dataStart):
-                xArray.append(lineArray[0])
-                yArray.append(lineArray[1])
-        dataArray.append(xArray)
-        dataArray.append(yArray)
-        return (dataArray)
+            lineArray = eachLine.split(self.splitter)
+            if (lineNumber == labelsRow):
+                self.labelsArray.append(lineArray[0])
+                self.labelsArray.append(lineArray[1])
+            elif (lineNumber >= dataStart):
+                self.xArray.append(lineArray[0])
+                self.yArray.append(lineArray[1])
         dataFile.close()
 
-    def labels(self, path=str(), xLabelText=str(), yLabelText=str(), labelLine=int(), splitter=str()):
-        dataFile = open(path, "r")
-        lineNumber = int(0)
-        labelArray = []
-        for eachLine in dataFile:
-            lineNumber += 1
-            if(lineNumber == labelLine):
-                lineArray = eachLine.split(splitter)
-                XLabel = str(xLabelText + " [" + lineArray[0] + "]")
-                YLabel = str(yLabelText + " [" + lineArray[1] + "]")
+    def labels(self, xLabelText=str(), yLabelText=str()):
+                XLabel = str(xLabelText + " [" + self.labelsArray[0] + "]")
+                YLabel = str(yLabelText + " [" + self.labelsArray[1] + "]")
                 labelArray = [XLabel, YLabel]
         return (labelArray)
 
-    def nameOfFile(self, i):
-        pathStr = pathsArray[i]
-        pathArray = pathStr.split("/")
-        name = pathArray[len(pathArray) - 1]
-        return (name)
+    def returnData(self):
+        dataArray = [self.xArray, self.yArray]
+        return(dataArray)
+
+    def pathsToFiles(self):
+        return (self.pathsToFiles)
+
+class graphComposer:
+
+    def __init__(self):
+        self.generalArray = []
+
+    def addCurve(self, dataArray):
+        self.generalArray.append(dataArray)
+
+    def addText(self, title, labelsArray):
+        self.labelsArray = labelsArray
+        self.title = title
+
+    def composeGraph(self):
+        plt.xlabel(self.labelsArray[0])
+        plt.ylabel(self.labelsArray[1])
+        plt.title(self.title)
+
+        pathsArray = data.pathsToFiles()
+
+        i = int(0)
+        j = int(0)
+        while(i < len(generalArray)):
+            dataArray = self.generalArray[i]
+            xArray = dataArray[j]
+            yArray = dataArray[j + 1]
+            nameArray = pathsArray[i].split("/")
+            name = nameArray[len(nameArray) - 1]
+            plt.plot(xArray, yArray, label = ("Source: " + name))
+            i += 1
+
+    def showGraph():
+        plt.legend()
+        plt.show()
 
 
-def plotResults():
+def main():
 
-    inp = getInput()
-    data = processData()
-    paths = inp.readInput()
-    generalArray = []
-    title = inp.title()
-    labelsArray = inp.labelsText()
-    xLabel = labelsArray[0]
-    yLabel = labelsArray[1]
-    splitter = inp.splitter()
-    dataStart = inp.dataStart()
-    unitRow = inp.labelsRow()
+    data = processData("\t")
+    g = graphComposer()
 
     firstOpened = bool(True)
-
     for eachElement in paths:
-        dataArray = data.parseValues(eachElement, splitter, dataStart)
-        generalArray.append(dataArray)
+        data.parseData(8, 6, eachElement)
+        dataArray = data.returnData()
+        g.addCurve(dataArray)
         if (firstOpened == True):
-            labelsArray = data.labels(eachElement, xLabel, yLabel, unitRow, splitter)
-            plt.xlabel(labelsArray[0])
-            plt.ylabel(labelsArray[1])
+            labelsArray = data.labels("Time", "CO2 Concentration")
+            g.addText("Garph of concentration of CO2", labelsArray)
             firstOpened = False
+    g.composeGraph()
+    g.showGraph()
 
-    i = int(0)
-    j = int(0)
-    while(i < len(generalArray)):
-        dataArray = generalArray[i]
-        xArray = dataArray[j]
-        yArray = dataArray[j + 1]
-        name = data.nameOfFile(i)
-        plt.plot(xArray, yArray, label = ("Source: " + name))
-        i += 1
-
-    plt.title(title)
-    plt.legend()
-    plt.show()
 plotResults()
